9ed8041d4672a6b48935d15ce07a0da7e2a218ef
atareversei (vivobook) <atareversei@gmail.com>
2025-01-19 23:20:13 +0330

    feat: add fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,21 @@
 <script lang="ts">
+	import '$lib/styles/typography.css'
+
 	import Header from '$lib/components/header/Header.svelte'
 	import { i18n } from '$lib/i18n'
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit'
 	let { children } = $props()
 </script>
+
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<!-- TODO - research subset=latin's effectiveness -->
+	<link
+		href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300..700&family=Fira+Sans:ital,wght@0,400;0,700;0,900;1,400;1,700;1,900&display=swap&subset=latin"
+		rel="stylesheet"
+	/>
+</svelte:head>
 
 <ParaglideJS {i18n}>
 	<Header />
