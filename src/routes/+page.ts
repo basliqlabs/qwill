f1e42113012ca8ff60501779cae319812446dab7
atareversei (vivobook) <atareversei@gmail.com>
2025-04-13 07:31:47 +0330

    feat: add simple landing page

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -3,7 +3,7 @@ import type { PaginatedData } from '$lib/utils/pagination'
 import type { Post } from 'content/config/posts'
 
 export async function load({ fetch, url }) {
-  const response = await fetch(`/api/${i18n.getLanguageFromUrl(new URL(url))}/blog/list/1`)
+  const response = await fetch(`/api/${i18n.getLanguageFromUrl(new URL(url))}/posts/list/1`)
   const posts: PaginatedData<Post> = await response.json()
   return { posts }
 }
