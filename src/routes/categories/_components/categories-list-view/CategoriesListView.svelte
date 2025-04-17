52b9d0ae529d46fb2ec5d77775d97a025d060910
atareversei (vivobook) <atareversei@gmail.com>
2025-04-17 20:21:45 +0330

    refactor: extract duplicated code

---
+++ src/routes/categories/_components/categories-list-view/CategoriesListView.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  import * as m from '$lib/paraglide/messages'
+  import Pagination from '$lib/components/pagination/Pagination.svelte'
+  import CategoryCard from '$lib/components/category-card/CategoryCard.svelte'
+  import type { PaginatedData } from '$lib/utils/pagination'
+  import type { Category } from 'content/config/categories'
+
+  type Props = {
+    categories: PaginatedData<Category>
+  }
+
+  let { categories }: Props = $props()
+  const categoriesList = $derived(categories.data)
+  const totalPages = $derived(categories.totalPages)
+</script>
+
+<div class="header">
+  <h1>{m.header_categories()}</h1>
+</div>
+
+<div class="category-list">
+  {#each categoriesList as category}
+    <CategoryCard {category} />
+  {/each}
+</div>
+
+<Pagination {totalPages} currentPage={1} baseLink="/categories/list" />
+
+<style>
+  .header {
+    display: flex;
+    flex-direction: column;
+    gap: var(--ws-3);
+  }
+
+  .category-list {
+    display: flex;
+    flex-direction: column;
+    gap: var(--ws-6);
+  }
+</style>
